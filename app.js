2538ea233b35c5db22d5adcac9ca52ecab55e170
Hidetaro7 <hidetaro7@gmail.com>
2020-12-16 21:27:04 +0900

    文字エフェクト大体できた

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.set('view engine', 'pug');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const featuredMessage = {
+  id: null,
+  lastUpdate: 0
+}
+const limitTime = 10000; //10秒おきに中心になるメッセージを入れ替える
+
 app.get('/', (req, res) => {
   res.render('index.pug');
 });
@@ -30,13 +36,17 @@ app.get('/viewer', (req, res) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const sendMessage = {centerd: false, message: msg.message}
+    //console.log(new Date().getTime() , featuredMessage.lastUpdate ,new Date().getTime() - featuredMessage.lastUpdate )
+    if(!featuredMessage.id || new Date().getTime() - featuredMessage.lastUpdate > limitTime) {
+      featuredMessage.id = msg.id;
+      sendMessage.centerd = true
+      featuredMessage.lastUpdate = new Date().getTime()
+    }
+    io.emit('chat message', sendMessage);
   });
   socket.on('good emitted', (msg) => {
     io.emit('good emitted', msg);
-  });
-  socket.on('shake', (msg) => {
-    io.emit('shake', msg);
   });
 });
 
@@ -48,5 +58,21 @@ if (module === require.main) {
   });
 }
 // [END appengine_websockets_app]
+
+// ここからテスト
+const testEmit = () => {
+  const texts = [
+    "みんなは場合どうかこの把持っ放しというののために這入り",
+    "話しいうませあっので",
+    "ですのはまして","事実を同時に","ですましませ。","何とも嘉納さんで","存在性質まだ説明","を知らでし世間その国","家私か攻撃のという","ごまごま","ごありなんたと、","同じほかはあれか","原因男が思いが、","岡田君のものに","香の私","がせっかく","お相違というて","誰個人","に実焦燥の","得ように","どうもご周旋が","見えるですでて","たといなおお出かけに","閉じうてみたのに考えますで。","しかしまたご必竟が","押しのはどう","自由と明らめたて、","その足のは","読むでしょけ","れどもという","個性にして","来たらたい。"
+  ];
+  setInterval(() => {
+    if(Math.random() < .6) {
+      io.emit('chat message', {id: 0, message:texts[Math.floor(Math.random() * texts.length)]});
+    }
+  },500)
+}
+testEmit()
+// ここまでテスト
 
 module.exports = server
